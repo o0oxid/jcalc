eb1c73263b65c173663f765f8cd73366b66242ef
Oleg Khoruzhenko <Oleg.Khoruzhenko@bazaarvoice.com>
2017-01-12 07:55:26 -0600

    junit test attempt

--- src/test/java/com/mycompany/jcalc/AppTest.java
+++ src/test/java/com/mycompany/jcalc/AppTest.java
@@ -1,38 +1,36 @@
 package com.mycompany.jcalc;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import java.util.LinkedList;
 
 /**
- * Unit test for simple App.
+ * Unit test for jcalc App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+class AppTest {
+    @Test
+    @DisplayName("My 1st JUnit 5 test! 😎 \uD83D\uDE31")
+    void optionSimpleCalculateTest(TestInfo testInfo) {
+        String expressionString = "1+2+((-380-1.3/3.15*577)+800)/2.3";
+        OptionParser parser = new OptionParser(expressionString);
+        LinkedList<ExpressionItem> expressionList = parser.getExpression();
+        Calculate calculator = new Calculate();
+        calculator.calculate(expressionList);
+        assertEquals(82.07522429261562,expressionList.get(0).perform());
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    @DisplayName("Priority operation test")
+    void optionSimpleCalculateTest(TestInfo testInfo) {
+        String expressionString = "1+2+((-380-1.3/3.15*577)+800)/2.3";
+        OptionParser parser = new OptionParser(expressionString);
+        LinkedList<ExpressionItem> expressionList = parser.getExpression();
+        Calculate calculator = new Calculate();
+        calculator.calculate(expressionList);
+        assertEquals(82.07522429261562,expressionList.get(0).perform());
     }
 }
